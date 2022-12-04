0a90a0e6997800e1d7ad1434f3d33915a4abce53
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-12-04 21:10:37 +0300

    loading component created. :fairy:

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,51 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import ArtistCard from "../components/ArtistCard";
+import Loading from "../components/Loading";
+
 export default function Home() {
+  const [ artistsData, setArtistsData ] = useState( null );
+  const [ loading, setLoading ] = useState( false );
+  
+  useEffect(() => {
+    (
+      async () => {
+        setLoading( true );
+        
+        try {
+          const res = await axios.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=b53230ab06865d451bea8e5aca7ea5e7&format=json');
+          
+          if( res.status !== 200 ) {
+            alert( res.statusText );
+            setLoading( false );
+          }
+          
+          const { artists } = res.data;
+          setArtistsData( artists );
+          setLoading( false );
+          
+        } catch (e) {
+          alert( e.message )
+        }
+      }
+    )()
+  },[]);
+  
+  console.log( artistsData )
+  
   return (
-    <>arda</>
+    <>
+      { artistsData?.artist.map( ( artist, index ) => {
+        return loading ?
+          <Loading/> :
+          <ArtistCard
+            key={index}
+            artistName={artist.name}
+            image={artist.image[2]['#text'] }
+            listeners={artist.listeners}
+            playCount={artist.playcount}
+          />;
+      }) }
+    </>
   )
 }
