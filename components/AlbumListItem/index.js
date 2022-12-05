9b6f8488b0a65218e3c27a5099f4a331bd745dc0
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-12-05 07:19:16 +0300

    button fixed for urls. :microbe:

--- components/AlbumListItem/index.js
+++ components/AlbumListItem/index.js
@@ -15,7 +15,11 @@ const AlbumListItem = ({ listData, index }) => {
             <span className="text-xs">playcount :{ listData?.playcount }</span>
         </span>
       </div>
-      <Button url={listData?.url}></Button>
+      <Button url={listData?.url}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+        </svg>
+      </Button>
     </li>
   )
 }
--- components/Button/index.js
+++ components/Button/index.js
@@ -2,7 +2,7 @@ const Button = ({size, children, url }) => {
   return(
     <button
       onClick={ () => window.open(url , '_blank') }
-      className={` bg-red-400 font-bold text-white px-6 py-2 ${size} `}
+      className={` bg-purple-200 rounded-xl font-bold text-white px-3 ${size} `}
     >
     {children}
   </button>)
